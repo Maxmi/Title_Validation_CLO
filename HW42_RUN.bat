081ff83168a199054f26e435aacfbc604f1df2b4
Yurii Chukhrai <chuhray.uriy@gmail.com>
2016-04-12 16:23:02 -0700

    Change HW42_RUN.bat-> git clone

--- HW42_RUN.bat
+++ HW42_RUN.bat
@@ -29,53 +29,46 @@ ECHO Maven installed
 CALL git --version > nul 2>&1
 IF NOT %ERRORLEVEL% == 0 GOTO EXIT_GIT
 ECHO Git installed
-IF NOT EXIST C:\%WS_DIR% GOTO NO_WORKSPACE
+
 
 GOTO NEXT
 
-::NEXT
-::IF NOT EXIST C:\%WS_DIR% GOTO NO_WORKSPACE
-::IF EXIST C:\%WS_DIR%\%REPO_NAME% RMDIR /S /Q C:\%WS_DIR%\%REPO_NAME%
-::CD C:\%WS_DIR%
-::git clone https://github.com/%GITHUB_ACCOUNT%/%REPO_NAME%.git
-
-::CD %REPO_NAME%
-::SLEEP 2
-
-
 :NEXT
-CD C:\%WS_DIR%\%REPO_NAME%
+IF NOT EXIST C:\%WS_DIR% GOTO NO_WORKSPACE
+IF EXIST C:\%WS_DIR%\%REPO_NAME% RMDIR /S /Q C:\%WS_DIR%\%REPO_NAME%
+CD C:\%WS_DIR%
+git clone https://github.com/%GITHUB_ACCOUNT%/%REPO_NAME%.git
 
+CD %REPO_NAME%
+SLEEP 2
 ECHO Building package Fat jar ...
 ::CALL mvn clean package -Dyc.version="%APP_VERSION%" -Dyc.mainClass="%MAIN_CLASS%" > report_HW42-%APP_VERSION%-%MAIN_CLASS%-Maven.txt
 CALL mvn clean package -Dyc.version="%APP_VERSION%" > report_HW42-%REPO_NAME%-%APP_VERSION%-Maven.txt
-DEL .\target\%REPO_NAME%-%APP_VERSION%.jar
-COPY .\target\%REPO_NAME%-%APP_VERSION%-jar-with-dependencies.jar .\target\Fat-%APP_VERSION%.jar
+
+DEL  C:\%WS_DIR%\%REPO_NAME%\target\%REPO_NAME%-%APP_VERSION%.jar
+COPY C:\%WS_DIR%\%REPO_NAME%\target\%REPO_NAME%-%APP_VERSION%-jar-with-dependencies.jar C:\%WS_DIR%\%REPO_NAME%\target\Fat-%APP_VERSION%.jar
 
 ECHO String - Command Line Options
 ECHO Executing Java programm Selenium Web driver ...
-java -cp C:\%WS_DIR%\%REPO_NAME%\target\Fat-%APP_VERSION%.jar %MAIN_CLASS_01% -h >> C:\%WS_DIR%\%REPO_NAME%\report_HW42-String-%APP_VERSION%-%MAIN_CLASS_01%-Runner.txt
-java -cp C:\%WS_DIR%\%REPO_NAME%\target\Fat-%APP_VERSION%.jar %MAIN_CLASS_01% -c TC-001.01 -u http://www.youtube.com -t YouTube >> C:\%WS_DIR%\%REPO_NAME%\report_HW42-String-%APP_VERSION%-%MAIN_CLASS_01%-Runner.txt
-java -cp C:\%WS_DIR%\%REPO_NAME%\target\Fat-%APP_VERSION%.jar %MAIN_CLASS_01% -c TC-001.02 -u http://www.youtube.com -t abc >> C:\%WS_DIR%\%REPO_NAME%\report_HW42-String-%APP_VERSION%-%MAIN_CLASS_01%-Runner.txt
+java -cp C:\%WS_DIR%\%REPO_NAME%\target\Fat-%APP_VERSION%.jar %MAIN_CLASS_01% -h >> report_HW42-String-%APP_VERSION%-%MAIN_CLASS_01%-Runner.txt
+java -cp C:\%WS_DIR%\%REPO_NAME%\target\Fat-%APP_VERSION%.jar %MAIN_CLASS_01% -c TC-001.01 -u http://www.youtube.com -t YouTube >> report_HW42-String-%APP_VERSION%-%MAIN_CLASS_01%-Runner.txt
+java -cp C:\%WS_DIR%\%REPO_NAME%\target\Fat-%APP_VERSION%.jar %MAIN_CLASS_01% -c TC-001.02 -u http://www.youtube.com -t abc >> report_HW42-String-%APP_VERSION%-%MAIN_CLASS_01%-Runner.txt
 
 ECHO Executing Java programm HtmlUnit driver ...
-java -cp C:\%WS_DIR%\%REPO_NAME%\target\Fat-%APP_VERSION%.jar %MAIN_CLASS_02% -h >> C:\%WS_DIR%\%REPO_NAME%\report_HW42-String-%APP_VERSION%-%MAIN_CLASS_02%-Runner.txt
-java -cp C:\%WS_DIR%\%REPO_NAME%\target\Fat-%APP_VERSION%.jar %MAIN_CLASS_02% -c TC-002.01 -u https://www.wikipedia.org -t Wikipedia >> C:\%WS_DIR%\%REPO_NAME%\report_HW42-String-%APP_VERSION%-%MAIN_CLASS_02%-Runner.txt
-java -cp C:\%WS_DIR%\%REPO_NAME%\target\Fat-%APP_VERSION%.jar %MAIN_CLASS_02% -c TC-002.02 -u https://www.wikipedia.org -t abc >> C:\%WS_DIR%\%REPO_NAME%\report_HW42-String-%APP_VERSION%-%MAIN_CLASS_02%-Runner.txt
+java -cp C:\%WS_DIR%\%REPO_NAME%\target\Fat-%APP_VERSION%.jar %MAIN_CLASS_02% -h >> report_HW42-String-%APP_VERSION%-%MAIN_CLASS_02%-Runner.txt
+java -cp C:\%WS_DIR%\%REPO_NAME%\target\Fat-%APP_VERSION%.jar %MAIN_CLASS_02% -c TC-002.01 -u https://www.wikipedia.org -t Wikipedia >> report_HW42-String-%APP_VERSION%-%MAIN_CLASS_02%-Runner.txt
+java -cp C:\%WS_DIR%\%REPO_NAME%\target\Fat-%APP_VERSION%.jar %MAIN_CLASS_02% -c TC-002.02 -u https://www.wikipedia.org -t abc >> report_HW42-String-%APP_VERSION%-%MAIN_CLASS_02%-Runner.txt
 
 
 ECHO List - Command Line Options
 ECHO Executing Java programm Selenium Web driver...
-java -cp C:\%WS_DIR%\%REPO_NAME%\target\Fat-%APP_VERSION%.jar %MAIN_CLASS_03% -h >> C:\%WS_DIR%\%REPO_NAME%\report_HW42-List-%APP_VERSION%-%MAIN_CLASS_03%-Runner.txt
-java -cp C:\%WS_DIR%\%REPO_NAME%\target\Fat-%APP_VERSION%.jar %MAIN_CLASS_03% -l "http://www.youtube.com|YouTube" "https://www.wikipedia.org|Wikipedia" "http://git-scm.com|Git" "http://www.apple.com|Apple" "https://www.google.com|Google" "https://www.ups.com/index.html|Delivery Service from UPS" "https://rexter.com|Rexter" "http://www.youtube.com|A" "https://www.wikipedia.org|B" "http://git-scm.com|C" "http://www.apple.com|D" "https://www.google.com|E" "https://www.ups.com/index.html|F" "https://rexter.com|G" >> C:\%WS_DIR%\%REPO_NAME%\report_HW42-List-%APP_VERSION%-%MAIN_CLASS_03%-Runner.txt
+java -cp C:\%WS_DIR%\%REPO_NAME%\target\Fat-%APP_VERSION%.jar %MAIN_CLASS_03% -h >> report_HW42-List-%APP_VERSION%-%MAIN_CLASS_03%-Runner.txt
+java -cp C:\%WS_DIR%\%REPO_NAME%\target\Fat-%APP_VERSION%.jar %MAIN_CLASS_03% -l "http://www.youtube.com|YouTube" "https://www.wikipedia.org|Wikipedia" "http://git-scm.com|Git" "http://www.apple.com|Apple" "https://www.google.com|Google" "https://www.ups.com/index.html|Delivery Service from UPS" "https://rexter.com|Rexter" "http://www.youtube.com|A" "https://www.wikipedia.org|B" "http://git-scm.com|C" "http://www.apple.com|D" "https://www.google.com|E" "https://www.ups.com/index.html|F" "https://rexter.com|G" >> report_HW42-List-%APP_VERSION%-%MAIN_CLASS_03%-Runner.txt
 
 
 ECHO Executing Java programm HtmlUnit driver...
-java -cp C:\%WS_DIR%\%REPO_NAME%\target\Fat-%APP_VERSION%.jar %MAIN_CLASS_04% -h >> C:\%WS_DIR%\%REPO_NAME%\report_HW42-List-%APP_VERSION%-%MAIN_CLASS_04%-Runner.txt
-java -cp C:\%WS_DIR%\%REPO_NAME%\target\Fat-%APP_VERSION%.jar %MAIN_CLASS_04% -l "http://www.youtube.com|YouTube" "https://www.wikipedia.org|Wikipedia" "http://git-scm.com|Git" "http://www.apple.com|Apple" "https://www.google.com|Google" "https://www.ups.com/index.html|Delivery Service from UPS" "https://rexter.com|Rexter" "http://www.youtube.com|A" "https://www.wikipedia.org|B" "http://git-scm.com|C" "http://www.apple.com|D" "https://www.google.com|E" "https://www.ups.com/index.html|F" "https://rexter.com|G" >> C:\%WS_DIR%\%REPO_NAME%\report_HW42-List-%APP_VERSION%-%MAIN_CLASS_04%-Runner.txt
-
-
-
+java -cp C:\%WS_DIR%\%REPO_NAME%\target\Fat-%APP_VERSION%.jar %MAIN_CLASS_04% -h >> report_HW42-List-%APP_VERSION%-%MAIN_CLASS_04%-Runner.txt
+java -cp C:\%WS_DIR%\%REPO_NAME%\target\Fat-%APP_VERSION%.jar %MAIN_CLASS_04% -l "http://www.youtube.com|YouTube" "https://www.wikipedia.org|Wikipedia" "http://git-scm.com|Git" "http://www.apple.com|Apple" "https://www.google.com|Google" "https://www.ups.com/index.html|Delivery Service from UPS" "https://rexter.com|Rexter" "http://www.youtube.com|A" "https://www.wikipedia.org|B" "http://git-scm.com|C" "http://www.apple.com|D" "https://www.google.com|E" "https://www.ups.com/index.html|F" "https://rexter.com|G" >> report_HW42-List-%APP_VERSION%-%MAIN_CLASS_04%-Runner.txt
 GOTO END
 
 :EXIT_JAVA
